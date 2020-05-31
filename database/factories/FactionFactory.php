87ad4d7c7c3911fe303ae9e1c28e9ae91cb2cee9
Justin Visser <justin@jhvisser.com>
2020-05-31 01:19:44 -0400

    Adding in crud generation

---
+++ database/factories/FactionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Faction;
+use Faker\Generator as Faker;
+
+$factory->define(Faction::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
