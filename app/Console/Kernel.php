a967b37d26fbe52049247523e2d56986ed6346a4
Justin Visser <justin@jhvisser.com>
2020-07-28 15:55:14 -0400

    Making telescope entries prune anything over 48 hours old

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->job(new UpdateAllFactionData)->hourly();
+        $schedule->command('telescope:prune --hours=48')->daily();
     }
 
     /**
