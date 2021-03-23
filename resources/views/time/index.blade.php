02568b8420db412652d9a06223c44c7631f699d5
Justin V <justin@jhvisser.com>
2021-03-23 19:47:14 -0400

    Making timecards into a class and more reusable/dynamic
    - This allows for easy support of adding new times to track such as
      by pulling in data from Torn API about user cooldowns as well as
      tracking custom inputs so that a user can add things like a chain

--- resources/views/time/index.blade.php
+++ resources/views/time/index.blade.php
@@ -7,53 +7,89 @@
 
 @push('scripts')
     <script type="text/javascript" charset="utf8">
+        class TimeCard {
+            constructor(id, title, hour = 0, minute = 0, second = 0, dayOfWeek = null,
+                        year = null, month = null, day = null) {
+                this.id = id;
+                this.title = title;
+                this.hour = hour;
+                this.minute = minute;
+                this.second = second;
+                this.dayOfWeek = dayOfWeek;
+                this.year = year;
+                this.month = month;
+                this.day = day;
+            }
+            createNextTime() {
+                let day = moment().utc()
+                    .hour(this.hour).minute(this.minute).second(this.second);
+                if (day.isBefore(moment().utc()))
+                    day.add(1, 'd');
+                if (this.dayOfWeek != null) {
+                    day.day(this.dayOfWeek);  // 0-6, 0 = Sunday, 1 = Monday, 6 = Saturday, etc...
+                    if (day.isBefore(moment().utc()))
+                        day.add(7, 'd');
+                }
+                if (this.year != null)
+                    day.year(this.year);
+                if (this.month != null && Number.isInteger(this.month))
+                    day.month(this.month - 1);
+                else if (this.month != null)
+                    day.month(this.month);
+                if (this.day != null)
+                    day.date(this.day);
+                return day;
+            }
+            get toString() {
+                moment.relativeTimeThreshold('h', 30);
+                moment.relativeTimeThreshold('m', 60);
+                moment.relativeTimeThreshold('s', 60);
+                let day = this.createNextTime();
+                let dateText = day.local().format("ddd [@] ha");
+                if (day.local().isSame(moment(), 'day'))
+                    dateText = day.local().format("[Today @] ha");
+                return day.fromNow()
+                    + "</br>" + dateText;
+            }
+        }
+
+        let cards = [
+            new TimeCard('company-day', 'Company Day', 18),
+            new TimeCard('torn-day', 'Torn Day'),
+            new TimeCard('company-week', 'Company Week', 18, 0, 0, 0),
+            new TimeCard('addiction-decay', 'Addiction Decay', 5, 30),
+            new TimeCard('chain', 'Chaining', 12, 0, 0, null, 2021, 03, 26)
+        ];
+
         $(document).ready( function () {
-            // Update all the times
+            // Generate and insert the HTML for the default time cards
+            createDefaultCards();
             updateTimes();
-            setInterval(updateTimes, 10000); //every 10 seconds update it.
+            setInterval(updateTimes, 7500); //every 10 seconds update it.
         });
 
         function updateTimes() {
             // Current time/date
             $("#datetime").text(moment().format("ddd, hh:mma"))
 
-            // New Company Day
-            $("#new-company-day").html(dateTimeToString(createNextTimeInDay(18)));
-
-            // New Torn Day
-            $("#new-torn-day").html(dateTimeToString(createNextTimeInDay()));
-
-            // New Company Week Date
-            $("#new-company-week").html(dateTimeToString(createNextDayInWeek(0, 18)));
-
-            // Addiction Decay
-            $("#addiction-decay").html(dateTimeToString(createNextTimeInDay(5, 30)));
+            cards.forEach(function (card) {
+                $('#' + card.id).html(card.toString);
+            });
         }
 
-        function createNextTimeInDay(hour = 0, minute = 0, second = 0) {
-            let day = moment().utc().hour(hour).minute(minute).second(second);
-            if (day.isBefore(moment().utc()))
-                day.add(1, 'd');
-            return day;
+        function createDefaultCards() {
+            cards.forEach(function (card) {
+                createNewCard(card.id, card.title);
+            });
         }
 
-        function createNextDayInWeek(dayOfWeek = 0, hour = 0, minute = 0, second = 0) {
-            let day = createNextTimeInDay(hour, minute, second);
-            day.day(dayOfWeek);  // 0-6, 0 = Sunday, 1 = Monday, 6 = Saturday, etc...
-            if (day.isBefore(moment().utc()))
-                day.add(7, 'd');
-            return day;
-        }
-
-        function dateTimeToString(momentTimeDate = moment().utc()) {
-            moment.relativeTimeThreshold('h', 30);
-            moment.relativeTimeThreshold('m', 60);
-            moment.relativeTimeThreshold('s', 60);
-            let dateText = momentTimeDate.local().format("ddd [@] ha");
-            if (momentTimeDate.local().isSame(moment(), 'day'))
-                dateText = momentTimeDate.local().format("[Today @] ha");
-            return momentTimeDate.fromNow()
-                + "</br>" + dateText;
+        function createNewCard(id, title) {
+            let html = "<div class=\"lg:w-1/3 px-3 pb-6\">\n" +
+                "<div class=\"bg-white p-5 rounded-lg shadow\">\n" +
+                "    <h2 class=\"text-2xl text-center\">"+ title +"</h2>\n" +
+                "    <p id=\""+ id +"\" class=\"text-center\"></p>\n" +
+                "</div></div>"
+            $("#card-holder").append(html);
         }
     </script>
 @endpush
@@ -63,34 +99,9 @@
 @section('content')
     <h1 class="text-4xl text-center pb-3">Torn Important Times Tracker</h1>
 
-    <main class="mx-auto w-2/3 md:min-w-50 box-border p-4 border-2 lg:flex lg:flex-wrap">
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-5 rounded-lg shadow">
-                <h2 class="text-2xl text-center">New Company Day</h2>
-                <p id="new-company-day" class="text-center"></p>
-            </div>
-        </div>
-
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-5 rounded-lg shadow">
-                <h2 class="text-2xl text-center">New Torn Day</h2>
-                <p id="new-torn-day" class="text-center"></p>
-            </div>
-        </div>
-
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-5 rounded-lg shadow">
-                <h2 class="text-2xl text-center">New Company Week</h2>
-                <p id="new-company-week" class="text-center"></p>
-            </div>
-        </div>
-
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-3 rounded-lg shadow">
-                <h2 class="text-2xl text-center">Natural Addiction Decay</h2>
-                <p id="addiction-decay" class="text-center"></p>
-            </div>
-        </div>
+    <main id="card-holder"
+          class="mx-auto w-2/3 md:min-w-50 box-border p-4 border-2 lg:flex lg:flex-wrap">
+        {{--    Dynamically inserted cards into here    --}}
     </main>
     <h2 class="text-center text-lg">Current local time: <span class="font-medium" id="datetime"></span></h2>
 
